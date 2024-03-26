ffc24625f08d50b4dd3991bb489d6cf25bec3ef4
trissz <triszten25@gmail.com>
2024-03-26 14:44:48 +0100

    finished first
    finished first

---
+++ CUNPO1_0326/CUNPO1/1. Feladat/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function ()
+{
+    $.getJSON('CUNPO1_konyv.json', function(data)
+    {
+        $.each(data.konyvek.konyv, function(i, param)
+        {
+            let tableRow = "<tr>" +
+                "<td>" + param.szerzo + "</td>" +
+                "<td>" + param.cim + "</td>" +
+                "<td>" + param.kiadas_eve + "</td>" +
+                "<td>" + param.kategoria + "</td>" +
+                "<td>" + param.nyelv + "</td>" +
+                "</tr>";
+
+            $("#book_data tbody").append(tableRow);
+            console.log(i);
+        });
+    });
+});
